7097d726a3f6284c8728339c04d0c9fbc739bcb9
galipyildiz <galipyildiz9606@outlook.com>
2023-11-29 09:20:23 +0300

    functions

--- helloworldinbrowser/app.js
+++ helloworldinbrowser/app.js
@@ -4,3 +4,27 @@ var heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
+function getTotal() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    var total = 0;
+    numbers.forEach(function (num) { return total += num; });
+    return total;
+}
+var heading1 = document.createElement('h1');
+heading1.textContent = "".concat(getTotal(1, 2, 3, 4, 5, 6, 7));
+document.body.appendChild(heading1);
+function add(a, b) {
+    return a + b;
+}
+var heading2 = document.createElement('h1');
+heading2.textContent = "".concat(add(1, 2));
+document.body.appendChild(heading2);
+var heading3 = document.createElement('h1');
+heading3.textContent = "".concat(add("ali", "veli"));
+document.body.appendChild(heading3);
+var heading4 = document.createElement('h1');
+heading4.textContent = "".concat(add("ali", 1));
+document.body.appendChild(heading4);
--- helloworldinbrowser/app.ts
+++ helloworldinbrowser/app.ts
@@ -4,3 +4,27 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading the document
 document.body.appendChild(heading);
+
+function getTotal(...numbers: number[]): number {
+    let total = 0;
+    numbers.forEach((num) => total += num);
+    return total;
+}
+let heading1 = document.createElement('h1');
+heading1.textContent = `${getTotal(1, 2, 3, 4, 5, 6, 7)}`;
+document.body.appendChild(heading1);
+
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: any, b: any): any {//dynamic variable type
+    return a + b;
+}
+
+let heading2 = document.createElement('h1');
+heading2.textContent = `${add(1, 2)}`;
+document.body.appendChild(heading2);
+
+let heading3 = document.createElement('h1');
+heading3.textContent = `${add("ali", "veli")}`;
+document.body.appendChild(heading3);
